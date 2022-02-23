6660f740f4629435d04e778cf251c7442e847d2b
Keli Booster <kbooster17@gmail.com>
2022-02-23 22:37:42 +0000

    add one more porject

--- components/Project.js
+++ components/Project.js
@@ -3,24 +3,22 @@ import Image from "next/image";
 
 const Project = ({ proj }) => {
   return (
-    <div className={[Style.proCard].join(" ")}>
+    <div className={[Style.proCard, Style.bgDusk, "px-2 py-2"].join(" ")}>
       <div className={[Style.proCardIcon].join(" ")}>
         <Image
           src={proj?.image}
-          layout="intrinsic"
-          width={257}
-          height={200}
+          layout="fill"
           alt="project image"
-          className={Style.proImg}
+          className={[Style.proImg].join(" ")}
         />
       </div>
       <a
         href={proj?.link}
-        className="text-lg mb-6 text-gray-200 hover:text-white"
+        className="text-lg mb-6 text-gray-300 underline hover:text-white"
       >
         {proj?.name}
       </a>
-      <div className="flex mt-6 flex-row flex-wrap justify-between text-md underline">
+      <div className="flex mt-6 flex-row flex-wrap justify-between text-md">
         {proj?.stack.map((stack) => (
           <span className="mr-2 mt-3">{stack}</span>
         ))}
